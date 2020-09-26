5d6ccaef0e69f316412679ef52913398ee4625e8
samahart <25089526+samahart@users.noreply.github.com>
2020-09-25 23:28:20 -0500

    Make image fit to screen
    This was done by setting body to position to fixed (this removed
    scroll bar) and making image width auto. Note: I might need to
    auto detect image size based and screen size and determine if
    width or height should be set to auto.

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ def home():
 
 
 if __name__ == "__main__":
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
     app.run(host='0.0.0.0', port=80)
