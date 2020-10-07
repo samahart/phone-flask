cd5511fbba52f462341dce34167a7fabc364489d
samahart <25089526+samahart@users.noreply.github.com>
2020-10-06 23:10:18 -0500

    Add api to get image from server

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file
+import os
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+FILE_TYPES = ('.png', '.jpeg', '.jpg')
+img_idx = -1
+images_path = os.path.join('static', 'temp')
+images = [os.path.join(images_path, img) for img in os.listdir(images_path) if img.endswith(FILE_TYPES) ]
+images_len = len(images)
 
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
+
+@app.route('/picture', methods=['GET'])
+def picture():
+    global img_idx
+    img_idx += 1
+    img_idx = img_idx % images_len
+    return send_file(images[img_idx])
 
 
 if __name__ == "__main__":
